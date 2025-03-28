c583ad4c9e911d1f9768a6e3e97d0be39bef8873
Nuzkito <nuzkito+github@gmail.com>
2025-03-28 21:43:51 +0100

    Add Ceuta and Melilla prices. Add Canary hours.

--- updatePrices.js
+++ updatePrices.js
@@ -10,11 +10,51 @@ dayjs.extend(timezone)
 
 const url = process.env.url ?? 'http://localhost:3000/_site'
 
-async function getPrices() {
+class GeoId {
+    #value
+
+    constructor(value) {
+        this.#value = value
+    }
+
+    value() {
+        return this.#value
+    }
+
+    static ceuta() {
+        return new GeoId(8744)
+    }
+
+    static peninsula() {
+        return new GeoId(8741)
+    }
+}
+
+class TimeZone {
+    #value
+
+    constructor(value) {
+        this.#value = value
+    }
+
+    value() {
+        return this.#value
+    }
+
+    static madrid() {
+        return new TimeZone('Europe/Madrid')
+    }
+
+    static canary() {
+        return new TimeZone('Atlantic/Canary')
+    }
+}
+
+async function getPrices(geoId) {
     const today = dayjs().tz('Europe/Madrid')
     const startDate = today.format('YYYY-MM-DD')
     const endDate = today.add(1, 'day').format('YYYY-MM-DD')
-    const url = `https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=${startDate}T00:00&end_date=${endDate}T00:00&time_trunc=hour`
+    const url = `https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=${startDate}T00:00&end_date=${endDate}T00:00&time_trunc=hour&geo_ids=${geoId.value()}`
 
     try {
         const response = await axios.get(url)
@@ -25,7 +65,7 @@ async function getPrices() {
     }
 }
 
-function processData(rawData) {
+function processData(rawData, timeZone) {
     const today = dayjs().tz('Europe/Madrid')
     const pvpcData = rawData.included.find(item => item.type === 'PVPC')
 
@@ -35,21 +75,19 @@ function processData(rawData) {
 
     const prices = pvpcData.attributes.values.map(item => {
         const datetime = dayjs(item.datetime).tz('Europe/Madrid')
-        if (datetime.format('YYYY-MM-DD') === today.format('YYYY-MM-DD')) {
+
+        if (datetime.isSame(today, 'day')) {
             return {
-                hour: datetime.format('HH'),
+                hour: datetime.tz(timeZone.value()).format('HH'),
                 kwh: Number((item.value / 1000).toFixed(4))
             }
         }
     }).filter(Boolean)
 
-    const thresholds = calculateThresholds(prices)
-    const pricesWithColors = prices.map(price => ({
+    return prices.map(price => ({
         ...price,
-        range: range(price.kwh, thresholds)
+        range: range(price.kwh, calculateThresholds(prices))
     }))
-
-    return pricesWithColors.sort((a, b) => a.hour.localeCompare(b.hour))
 }
 
 function calculateThresholds(prices) {
@@ -71,19 +109,16 @@ function range(kwh, thresholds) {
 }
 
 async function generateHtml(prices) {
-    const today = dayjs().tz('Europe/Madrid')
+    const currentDateTime = dayjs().tz('Europe/Madrid')
 
     try {
         const template = await fs.readFile('template.html', 'utf8')
-        const html = ejs.render(template, {
-            prices: prices,
-            currentDate: today.format('DD/MM/YYYY'),
-            currentTime: today.format('HH:mm'),
-            url: url,
-        })
+        const html = ejs.render(template, { prices, currentDateTime, url })
+
         await fs.mkdir('_site', { recursive: true })
         await fs.writeFile('_site/index.html', html)
         await fs.cp('resources', '_site', { recursive: true })
+
         console.log('HTML generated successfully')
     } catch (error) {
         console.error('Error generating HTML:', error.message)
@@ -93,9 +128,21 @@ async function generateHtml(prices) {
 
 async function updatePrices() {
     try {
-        const rawData = await getPrices()
-        const prices = processData(rawData)
-        await generateHtml(prices)
+        const peninsulaData = await getPrices(GeoId.peninsula())
+        const peninsulaPrices = processData(peninsulaData, TimeZone.madrid())
+
+        const ceutaData = await getPrices(GeoId.ceuta())
+        const ceutaPrices = processData(ceutaData, TimeZone.madrid())
+
+        const canariasPrices = processData(peninsulaData, TimeZone.canary())
+
+        const allPrices = {
+            peninsula: peninsulaPrices,
+            canarias: canariasPrices,
+            ceutaMelilla: ceutaPrices
+        }
+
+        await generateHtml(allPrices)
     } catch (error) {
         console.error('Error updating prices:', error.message)
         process.exit(1)
